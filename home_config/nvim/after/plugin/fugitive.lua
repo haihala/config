065a0a55de1719927c13b834f53d713ee68d8d9c
Eero Häihälä <eero.haihala@vincit.fi>
2023-08-15 16:35:00 +0300

    Add pushing shortcut

--- home_config/nvim/after/plugin/fugitive.lua
+++ home_config/nvim/after/plugin/fugitive.lua
@@ -4,3 +4,4 @@ vim.keymap.set("n", "<leader>gb", ":Git blame<CR>")
 vim.keymap.set("n", "<leader>ga", ":Gwrite<CR>")
 vim.keymap.set("n", "<leader>gu", ":Gread<CR>") -- "Undo"
 vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
+vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
