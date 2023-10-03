b4ed7814afe41ce907ad4f129e0dcbed63f58afd
Eero H??ih??l?? <eero.haihala@gmail.com>
2023-10-03 20:33:21 +0300

    Remove unnecessary return

--- home_config/nvim/lua/packer-config.lua
+++ home_config/nvim/lua/packer-config.lua
@@ -1,6 +1,6 @@
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
+require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
