3aaec083daa815a29386f6755d43a9db9ce81301
Eero Häihälä <eero.haihala@vincit.fi>
2023-11-24 13:55:39 +0200

    Add ability to ignore gitignore with telescope pickers

--- home_config/nvim/after/plugin/telescope.lua
+++ home_config/nvim/after/plugin/telescope.lua
@@ -20,20 +20,24 @@ telescope.setup {
 }
 
 telescope.load_extension("ui-select")
-telescope.load_extension("file_browser")
+local fb = telescope.load_extension("file_browser")
 local projects = telescope.load_extension('projects')
 
 vim.keymap.set('n', '<leader>fP', projects.projects, {})
 
 -- File explorer (comes from the file browser extention)
-vim.keymap.set("n", "<leader>fe", ":Telescope file_browser path=%:p:h<CR>")
-vim.keymap.set("n", "<leader>fr", ":Telescope file_browser <CR>") -- r = root
+vim.keymap.set("n", "<leader>fe", function() fb.file_browser({ path = "%:p:h" }) end, {})
+vim.keymap.set("n", "<leader>fE", function() fb.file_browser({ path = "%:p:h", hidden = true, no_ignore = true }) end, {})
+vim.keymap.set("n", "<leader>fr", fb.file_browser, {})
+vim.keymap.set("n", "<leader>fR", function() fb.file_browser({ hidden = true, no_ignore = true }) end, {})
 
 -- Find files
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fF', function() builtin.find_files({ no_ignore = true }) end, {})
 
 -- Search
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fS', function() builtin.live_grep({ additional_args = { "--no-ignore" } }) end, {})
 
 -- Jumps
 vim.keymap.set('n', '<leader>fj', builtin.jumplist, {})
