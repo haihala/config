0b23a653857dfd8ae3c18087d16d3d1c0e356dca
Eero Häihälä <eero.haihala@vincit.fi>
2023-12-17 08:24:23 +0200

    Improve LSP keymaps

--- home_config/nvim/after/plugin/lsp.lua
+++ home_config/nvim/after/plugin/lsp.lua
@@ -31,18 +31,17 @@ local picker = require("telescope.builtin")
 
 lsp_zero.on_attach(function(client, bufnum)
     local opts = { buffer = bufnum, remap = false }
-
     -- Info popup
     vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover, opts) -- Inspect
 
     -- Jump
-    vim.keymap.set("n", "<leader>jd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "<leader>jr", function() picker.lsp_references({ include_declaration = false, }) end, opts)
-    vim.keymap.set("n", "<leader>js", picker.lsp_document_symbols, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gr", function() picker.lsp_references({ include_declaration = false, }) end, opts)
+    vim.keymap.set("n", "<leader>fs", picker.lsp_document_symbols, opts) -- find symbols
 
     -- Diagnostics
-    vim.keymap.set("n", "<leader>di", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "<leader>dl", picker.diagnostics, opts)
+    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "<leader>fd", picker.diagnostics, opts) -- Find diagnostics
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 
