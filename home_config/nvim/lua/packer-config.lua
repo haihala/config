815bfd152b1e0d96072ab216e71621e22545ad5a
Eero Häihälä <eero.haihala@gmail.com>
2024-07-30 08:48:26 +0300

    Add jsregexp install to luasnip

--- home_config/nvim/lua/packer-config.lua
+++ home_config/nvim/lua/packer-config.lua
@@ -56,7 +56,10 @@ require('packer').startup(function(use)
             -- Autocompletion
             { 'hrsh7th/nvim-cmp' },
             { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
+            {
+                'L3MON4D3/LuaSnip',
+                run = "make install_jsregexp"
+            },
         }
     }
 
