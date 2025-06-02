0f6fabae1461c64d5dd9324b518f0578fa693319
Eero Häihälä <eero.haihala@gmail.com>
2025-06-02 22:24:43 +0300

    Strip down local Avante model, update nvim packages

--- home_config/nvim/lua/plugins/avante.lua
+++ home_config/nvim/lua/plugins/avante.lua
@@ -3,8 +3,6 @@ return {
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
     opts = {
-        -- add any opts here
-        -- for example
         provider = "openai",
         openai = {
             endpoint = "https://api.openai.com/v1",
@@ -15,21 +13,12 @@ return {
             --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
         },
     },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
         "stevearc/dressing.nvim",
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
-        --- The below dependencies are optional,
-        "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-        "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-        "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-        "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-        "zbirenbaum/copilot.lua",        -- for providers='copilot'
         {
             -- support for image pasting
             "HakonHarnes/img-clip.nvim",
